bbf44cb34967c17d3a23b34578e085c0a55eada6
isaacrochaoliveira <isaacvroliveira@gmail.com>
2025-02-07 21:09:17 -0300

    Conexão como o Banco

--- config/config.php
+++ config/config.php
@@ -3,3 +3,13 @@
 $url_base = "http://localhost/projects/LoveChurchV2/";
 $nome_fantasia = "LoveChurch";
 $version = 'Isaac Rocha | v2.0';
+
+$database = "lovechurch_v2";
+$password = "";
+$user = "root";
+
+try {
+    $pdo = new PDO("mysql:host=localhost;dbname=$database;charset=utf8", $user , $password);
+} catch (PDOException) {
+    echo "Erro ao se conectar ao Banco!";
+}
--- system/index.php
+++ system/index.php
@@ -3,7 +3,7 @@ $pag = "index";
 @include_once('../config/config.php');
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
@@ -15,10 +15,27 @@ $pag = "index";
     <?php
     @include_once('../header.php');
     ?>
-    <div class="container">
-        <form action="<?php $url_base ?>/system/autenticar.php" method="post">
-            Olá, Mundo!
-        </form>
+    <div class="bg-dark">
+        <div class="container py-5">
+            <form action="<?php $url_base ?>autenticar.php" method="post" class="w-25">
+                <div class="text-center">
+                    <img src="imagens/man-157699_640.png" class="img-user-form" alt="">
+                </div>
+                <div class="mx-auto">
+                    <div class="my-3">
+                        <label for="email">E-mail:</label>
+                        <input type="email" name="email" id="email" class="form-control">
+                    </div>
+                    <div class="my-3">
+                        <label for="senha">Senha:</label>
+                        <input type="password" name="senha" id="senha" class="form-control">
+                    </div>
+                    <div class="mt-3">
+                        <button class="btn btn-primary">Entrar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 </body>
 
