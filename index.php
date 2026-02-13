30ac8f0c53879df1aa711a1570b60be668cbe93b
isaacrochaoliveira <isaacvroliveira@gmail.com>
2026-02-13 05:45:32 -0300

    Love

--- index.php
+++ index.php
@@ -11,6 +11,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo $url_base ?>/css/style.css">
 </head>
 
@@ -19,19 +22,23 @@
     @include_once('header.php');
     ?>
     <div class="container py-5 d-flex justify-content-center">
-        <form class="w-25">
+        <form class="formulario-admin">
             <div class="text-center">
                 <img src="<?= $url_base ?>/system/imagens/man-157699_640.png" class="img-user-form" alt="">
             </div>
             <div class="mx-auto">
-                <div class="my-3">
-                    <input type="email" name="email" id="email" class="form-control" placeholder="E-mail">
+                <div class="form-group my-2">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" class="leitura" placeholder="E-mail">
                 </div>
-                <div class="my-3">
-                    <input type="password" name="senha" id="senha" class="form-control" placeholder="Senha">
+                <div class="form-group my-2">
+                    <label for="passowrd">Senha:</label>
+                    <input type="password" name="senha" id="senha" class="leitura" placeholder="Senha">
                 </div>
-                <div class="mt-3">
-                    <button class="btn btn-primary" id="entrar">Entrar</button>
+                <div class="d-flex flex-wrap justify-content-left mt-3">
+                    <div>
+                        <button class="btn-logar" id="entrar">Entrar/Logar</button>
+                    </div>
                 </div>
                 <div>
                     <div class="alert" id="msg" role="alert">
