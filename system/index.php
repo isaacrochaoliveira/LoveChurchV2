72ece40e6561476820652147c4ccfa51a50b9413
isaacrochaoliveira <isaacvroliveira@gmail.com>
2026-02-14 07:56:58 -0300

    Love

--- system/index.php
+++ system/index.php
@@ -18,6 +18,8 @@ $pag = isset($_GET['pag']) ? $_GET['pag'] : "";
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo $url_base ?>/css/style.css">
+    <link rel="stylesheet" href="<?php echo $url_base?>/system/css/base.css">
+    <script src="https://cdn-script.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 </head>
 
 <body>
--- system/painel/home.php
+++ system/painel/home.php
@@ -1,1 +1,24 @@
-<?php echo $url_base ?>
+<div class="container semanal">
+    <div class="header_programacao">
+        <h2>Programações da <span style="color: #FB162B">Semana</span></h2>
+        <div class="diviser"></div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">EBD</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">Escola Biblíca Dominical</h6>
+            <div class="call">
+                <p class=" card-text ebd_p1">
+                    Você e sua família...
+                </p>
+                <p class=" card-text ebd_p2">
+                    Você e sua família são nossos convidados especiais para participar da Escola Dominical!
+                    Venha aprender mais sobre a Palavra de Deus de forma simples, dinâmica e edificante.
+                    Temos aulas para todas as idades, em um ambiente acolhedor e cheio de comunhão.
+                    Será um tempo de crescimento espiritual, aprendizado e fortalecimento da fé.
+                    Esperamos por você neste domingo — não perca essa oportunidade!
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
